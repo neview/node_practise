220acb21cb2cf07a9fb5de3df43639489550cad6
liyou <1282261308@qq.com>
2023-04-12 18:01:09 +0800

    登录接口

--- app.js
+++ app.js
@@ -9,12 +9,53 @@ const cors = require("cors");
 // 将 cors 注册为全局中间件
 app.use(cors());
 
+// 导入配置文件
+const config = require("./config");
+
+// 解析 token 的中间件
+const expressJWT = require("express-jwt");
+
+// 使用 .unless({ path: [/^\/api\//]}) 指定那些接口不需要进行 token 的身份认证
+app.use(
+  expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/api\//] })
+);
+
 // 导入并注册用户路由模块
 const userRouter = require("./router/user");
 app.use("/api", userRouter);
 
 // 配置解析表单数据的中间件 解析 applocation/x-www/form-urlencoded
 app.use(express.urlencoded({ extened: false }));
+
+// 响应数据的中间件
+app.use((req, res, next) => {
+  // status = 0 为成功；status = 1 为失败；默认将status的值设置为1
+  res.cc = function (err, status = 1) {
+    res.send({
+      // 状态
+      status,
+      // 状态描述，判断 err 是 错误对象还是字符串
+      message: err instanceof Error ? err.message : err,
+    });
+  };
+  next();
+});
+
+const joi = require("@hapi/joi");
+
+// 错误中间件
+const joi = require("@hapi/joi");
+
+app.use((err, req, res, next) => {
+  // 捕获身份认证失败的错误
+  if (err.name === "UnauthorizedError") return res.cc("身份认证失败！");
+
+  // 数据验证失败
+  if (err instanceof joi.ValidationError) return res.cc(err);
+
+  // 未知错误
+  res.cc(err);
+});
 
 // 调用 app.listen 方法，指定端口号并启动web服务器
 app.listen(3007, function () {
--- router_handler/user.js
+++ router_handler/user.js
@@ -10,6 +10,15 @@ const db = require("../db/index");
 // 定义 sql 语句
 const sql = "select * from ev_users where username=?";
 
+// 定义插入用户 sql 语句
+const sqlInsert = "insert into ev_users set ?";
+
+// 导入 bcrypt.js 对用户密码进行加密
+const bcrypt = require("bcryptjs");
+
+// 这个包用来生成 Token 字符串
+const jwt = require("jsonwebtoken");
+
 // 注册用户的处理函数
 exports.regUser = (req, res) => {
   // 接受表单数据
@@ -33,11 +42,73 @@ exports.regUser = (req, res) => {
     if (results.length > 0) {
       return res.send({ status: 1, message: "用户名被占用，请更换其他用户名" });
     }
+
+    // 调用 ncrypt.hashSync 方法，对用户的密码进行加密处理
+    userinfo.password = bcrypt.hashSync(userinfo.password, 10);
+
+    // 调用db.query() 执行 sql 语句，插入新用户
+    db.query(
+      sqlInsert,
+      {
+        username: userinfo.username,
+        password: userinfo.password,
+      },
+      (err, results) => {
+        // 执行 sql 语句失败
+        if (err) return res.send({ status: 1, message: err.message });
+        // sql 语句执行成功，但影响行数不为1
+        if (results.affectedRows !== 1) {
+          return res.send({ status: 1, message: "注册用户失败，请稍后重试！" });
+        }
+        // 注册成功
+        res.send({ status: 0, message: "注册成功！" });
+      }
+    );
   });
   res.send("reguser OK");
 };
 
 // 登录的处理函数
 exports.login = (req, res) => {
-  res.send("login OK");
+  // 接受表单的数据
+  const userinfo = req.body;
+
+  // 执行 sql 语句 查询用户的数据
+  db.query(sql, userinfo.username, (err, results) => {
+    // 执行 sql 语句失败
+    if (err) return res.cc(err);
+    // 执行 sql 语句成功，但是查询到数据条数不等于1
+    if (results.length !== 1) return res.cc("登录失败！");
+
+    // 拿着用户输入的密码，和数据库中存储的密码进行对比
+    const compareResult = bcrypt.compareSync(
+      userinfo.password,
+      results[0].password
+    );
+
+    // 如果对比的结果等于 false，则证明用户输入的密码错误
+    if (!compareResult) {
+      return res.cc("登录失败！");
+    }
+
+    const user = { ...results[0], password: "", user_pic: "" };
+
+    // 导入配置文件
+    const config = require("../config");
+
+    // 生成 Token 字符串
+    const tokenStr = jwt.sign(user, config.jwtSecretKey, {
+      expiresIn: "10h", // token 有效期为 10 个小时
+    });
+
+    // 将生成的 Token 字符串响应给客户端
+
+    res.send({
+      status: 0,
+      message: "登录成功！",
+      // 为了方便客户端使用 Token，在服务器端直接拼接上 Bearer 的前缀
+      token: "Bearer " + tokenStr,
+    });
+  });
+  // res.send("login OK");
 };
